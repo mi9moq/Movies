e5cbf25e0f2fd55dbd82d2d755eb3af1b3d50868
Denis Mironov <denis5918@gmail.com>
2024-02-20 18:40:01 +0700

    добавил сущность для UI слоя

---
+++ app/src/main/java/com/mironov/movies/domain/entity/Movie.kt
@@ -0,0 +1,11 @@
+package com.mironov.movies.domain.entity
+
+import com.mironov.movies.data.remote.models.Genre
+
+data class Movie(
+    val id: Int,
+    val name: String,
+    val genres: List<Genre>,
+    val year: Int,
+    val posterUrl: String
+)
